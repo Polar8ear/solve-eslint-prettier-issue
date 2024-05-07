eff74665bedb1a518114e566d0fcee4ba7bd9b7d
Chuo <ignatius.chuo@seamlessx.com>
2024-05-07 16:00:12 +0800

    initial commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+const ShipmentItem = {
+  description: '',
+  itemValueCurrency: '',
+  itemValueInCents: '',
+  quantity: '',
+  weightInGrams: '',
+};
+
+// FIXME: No. Ugly AF. Coolness level 0
+const { description, itemValueCurrency, itemValueInCents, quantity, weightInGrams } =
+  ShipmentItem;
+
+// Awww yiss 5/5
+const {
+  description,
+  itemValueCurrency,
+  itemValueInCents,
+  quantity,
+  weightInGrams
+} = ShipmentItem;
